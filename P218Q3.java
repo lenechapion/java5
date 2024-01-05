97a5d059af59fa33f8935062dce80930bc1cdc1a
lenechapion <ryokitta326@gmail.com>
2024-01-05 20:29:39 +0900

    P218Q3 更新

--- P218Q3.java
+++ P218Q3.java
@@ -3,8 +3,16 @@ package java5;
 public class P218Q3 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	
+		String title = "タイトル";
+		String address = "qawsde@mail.com";
+		String text = "本文";
+		email(title, address, text);
 
 	}
-
+	public static void email(String title,String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名:" + title);
+		System.out.println("本文:" + text);
+	}
 }
